f36f83d8117de8024c867b45067a480413e60ae1
somephic <285255390+somephic@users.noreply.github.com>
2026-05-19 00:58:52 +0000

    feat: add vite transform and serve

--- packages/opui/plugins/vite.mjs
+++ packages/opui/plugins/vite.mjs
@@ -1,29 +1,80 @@
 // @ts-check
+import { readFile } from "node:fs/promises"
+import { dirname, resolve } from "node:path"
+import { fileURLToPath } from "node:url"
+import { createRequire } from "node:module"
+import postcss from "postcss"
+import atImport from "postcss-import"
 import { cosmiconfig } from "cosmiconfig"
+
+const require = createRequire(import.meta.url)
+const postcssRename = require("postcss-rename")
+
+const here = dirname(fileURLToPath(import.meta.url))
+const root = resolve(here, "..")
+
+const targets = {
+  "virtual:opui.css": "css/imports.css",
+  "virtual:opui-components.css": "css/components.css",
+  "virtual:opui-open-props.css": "open-props.css",
+}
 
 /**
  * @returns {import('vite').Plugin}
  */
 export const opui = () => ({
   name: "opui",
-  /**
-   * @param {string} id
-   * @returns {Promise<string | undefined>}
-   */
+
   async resolveId(id) {
-    if (id === "virtual:opui-config") return id
+    if (id === "virtual:opui-config" || id in targets) {
+      return id
+    }
   },
-  /**
-   * @param {string} id
-   * @returns {Promise<string | undefined>}
-   */
+
   async load(id) {
+    // import the config
     if (id === "virtual:opui-config") {
       const explorer = cosmiconfig("opui")
       const result = await explorer.search()
-      const config = result?.config || { prefix: "" }
+      const config = result?.config || {}
       return `export default ${JSON.stringify(config)}`
+    }
+
+    /**
+     * @param {string} id
+     * @returns {id is keyof typeof targets}
+     */
+    function isTargetId(id) {
+      return id in targets
+    }
+
+    // import and transform css targets
+    if (isTargetId(id)) {
+      const explorer = cosmiconfig("opui")
+      const result = await explorer.search()
+      const { prefix } = result?.config || {}
+
+      const inputPath = resolve(root, targets[id])
+      const css = await readFile(inputPath, "utf-8")
+
+      const processor = postcss(
+        [
+          atImport(),
+          prefix &&
+            postcssRename({
+              prefix,
+            }),
+        ].filter(Boolean),
+      )
+
+      const processed = await processor.process(css, {
+        from: inputPath,
+        to: id,
+      })
+
+      return processed.css
     }
   },
 })
+
 export default opui

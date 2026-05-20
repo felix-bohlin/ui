24167fcf0a8f39bb21936d4e5c3a5f7a0b251da3
somephic <285255390+somephic@users.noreply.github.com>
2026-05-20 13:55:37 +0000

    feat: add form examples

---
+++ src/component-examples/form/Divider.svelte
@@ -0,0 +1,26 @@
+<!--
+<script lang="ts">
+  import { Form } from "@opui/svelte"
+  import { FieldSet } from "@opui/svelte"
+  import { FieldLegend } from "@opui/svelte"
+  import { FieldGroup } from "@opui/svelte"
+  import { TextField } from "@opui/svelte"
+  import { Button } from "@opui/svelte"
+  import { Divider } from "@opui/svelte"
+</script>
+
+<Form>
+  <FieldSet>
+    <FieldLegend>Post Content</FieldLegend>
+    <FieldGroup>
+      <TextField label="Title" placeholder="My new post" />
+    </FieldGroup>
+  </FieldSet>
+
+  <Divider />
+
+  <FieldGroup>
+    <Button variant="filled">Publish</Button>
+  </FieldGroup>
+</Form>
+-->

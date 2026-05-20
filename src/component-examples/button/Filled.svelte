770009f9f40db5c50b56b021db83c63fd47a2b06
somephic <285255390+somephic@users.noreply.github.com>
2026-05-20 01:13:52 +0000

    feat: add button examples

---
+++ src/component-examples/button/Filled.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { Button } from "@opui/svelte"
+</script>
+
+<Button variant="filled">Filled</Button>
+<Button variant="filled" disabled>Disabled</Button>
+<Button variant="filled" href="#filled">Link</Button>
---
+++ src/component-examples/button/Outlined.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { Button } from "@opui/svelte"
+</script>
+
+<Button variant="outlined">Outlined</Button>
+<Button variant="outlined" disabled>Disabled</Button>
+<Button variant="outlined" href="#outlined">Link</Button>

917a3407fc3adb868d776cb2f63381b816ef4b26
Felix Bohlin <felix.bohlin@cambio.se>
2026-05-22 14:23:44 +0200

    chore: add missing changes

--- src/components/Example/index.ts
+++ src/components/Example/index.ts
@@ -1,12 +1,10 @@
 import Example from "./Example.astro"
 import Preview from "./Preview.astro"
-import PreviewSvelte from "./Preview.svelte"
 import Code from "./Code.astro"
 
-export { Preview, PreviewSvelte, Code }
+export { Preview, Code }
 
 export default Object.assign(Example, {
   Preview,
-  PreviewSvelte,
   Code,
 })

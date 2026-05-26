01c66f9e16363e71c5fbe4de9f4a1625df6da89e
Felix Bohlin <felix.bohlin@cambio.se>
2026-05-26 09:14:30 +0200

    chore: remove lint errors on component examples

--- svelte.config.js
+++ svelte.config.js
@@ -2,4 +2,8 @@ import { vitePreprocess } from "@astrojs/svelte"
 
 export default {
   preprocess: vitePreprocess(),
+  onwarn(warning, defaultHandler) {
+    if (warning.filename?.includes("component-examples")) return
+    defaultHandler(warning)
+  },
 }
